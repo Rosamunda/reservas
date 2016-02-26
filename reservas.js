6146a37801136929b1bc3bc2f237238bae990c1e
Rosamunda <Rosamunda@users.noreply.github.com>
2016-02-26 18:40:41 -0300

    Detalles menores
    "this.nombre(i)" me queda como undefined luego de apretar el botón de reserva.

--- reservas.js
+++ reservas.js
@@ -6,19 +6,23 @@ var reservasAPP = {
 			//stackoverflow.com/questions/35654523/js-why-is-the-var-not-declared-when-it-is-declared-inside-a-method-of-an-object#35654703
 			var impresionGrilla = $('#grilla').append("<div class=\"row col-md-5\"><span style=\"titulo\">"+i+":00 </span><span id=\"horario"+i+"\"><input type=\"text\" id=\"hora"+i+"\"></span>&nbsp;<button class=\"btn btn-primary btn-xs\" onclick=\"return reservasAPP.guardarReserva(" + i + ");\">Reservar</button>&nbsp;<button class=\"btn btn-danger btn-xs\" onclick=\"return reservasAPP.cancelarReserva(" +i+ ");\">Cancelar</button></div><br><br>");
 		};
+		console.log("Grilla impresa");
 	},
 	nombre: function (i) {
 		//por qué una función? 
 		//http://stackoverflow.com/questions/35565979/js-why-this-object-property-wont-work-and-it-only-works-if-i-put-it-inside-one
         return $('#hora'+i).val();
-
     },	
 	guardarReserva:function(i){
 		var reservaConfirmada = $('#horario'+i).html('--> '+this.nombre(i));
+		var name = this.nombre(i);
+		//porqué este console log no anda?
+		console.log("Reserva Confirmada en horario "+i+":00 horas para "+name);
 		return false;
 	},
 	cancelarReserva:function(i){
 		var reservaCancelada = $('#horario'+i).html("<input type=\"text\" id=\"horario"+i+"\">");
+		console.log("Reserva Cancelada en horario "+i+":00 horas para "+this.nombre(i));
 		//porqué return false?
 		return false;
 	},	
